ffd8c4d6928c1c46cc680b4b2e192e62fb153a22
YoshiKill <46250788+YoshiKill@users.noreply.github.com>
2026-05-07 22:25:31 +0300

    Update build exe from pyw.bat

--- build exe from pyw.bat
+++ build exe from pyw.bat
@@ -1,4 +1,4 @@
 @echo off
-pyinstaller --noconfirm --onefile --windowed --icon="app_icon.ico" MTProto_Harvester.pyw
+pyinstaller --noconfirm --onefile --windowed --icon="app_icon.ico" MTProto_Harvester.py
 echo ===== Done!! You are the Programist Congratulations =====
 pause >nul
